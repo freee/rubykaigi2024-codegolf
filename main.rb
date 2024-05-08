52080af78ccdd1321a5027b75180d5e622361f3d
yongi-k <yongi-kim@c-fo.com>
2024-05-08 17:25:16 +0900

    Add contents

---
+++ main.rb
@@ -0,0 +1,9 @@
+m = []
+26.times do
+  line = gets.split
+  m << [line[0], "\e[#{line[1]}m", line[2].to_i, line[3].to_i]
+end
+
+# ここを回答
+
+a.each { |row| puts row.map { |cell| "#{cell[1]}#{cell[0]}\e[0m" }.join }
