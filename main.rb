c1138eceedbed4c57a466d4d44f3ef27be73e511
yongi-k <yongi-kim@c-fo.com>
2024-05-09 18:57:58 +0900

    Add example code

--- main.rb
+++ main.rb
@@ -5,5 +5,21 @@ m = []
 end
 
 # ここを回答 Answer here
+# example：
+# initialize a map
+a = Array.new(20) { Array.new(16) { ["　", ""] } }
+# configure the map
+m.each do |mapping|
+  name = mapping[0] + "　　"
+  row = mapping[3] * 2
+  col = mapping[2] * 2
+  color = mapping[1]
+
+  # Place the string in a 2x2 block
+  a[row][col] = [color, name[0]].reverse
+  a[row][col + 1] = [color, name[1]].reverse
+  a[row + 1][col] = [color, name[2]].reverse
+  a[row + 1][col + 1] = [color, name[3]].reverse
+end
 
 a.each { |row| puts row.map { |cell| "#{cell[1]}#{cell[0]}\e[0m" }.join }
